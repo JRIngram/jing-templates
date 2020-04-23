31702c63029907eb451eadc0760a4d5f1109fe27
Jamie Ingram <jr-ingram@hotmail.co.uk>
2020-04-23 18:32:42 +0000

    Add yes_no_function

--- jing-templates.sh
+++ jing-templates.sh
@@ -5,16 +5,27 @@
 #   * Pull Request Template
 #   * Bug Template 
 #   * Feature request Template 
+
+yes_no_input () {
+    echo "$1 [Y/N]"
+    read answer
+}
+
 echo "####### jing-templates #######"
 echo "What is your github username?"
 read USERNAME 
+
 echo "$USERNAME, what is the absolute path to your repository?"
 read PATH
+echo $PATH
+
 echo "What is the project name?"
 read PROJECT
+echo $PROJECT
+
 echo "What is the URL of the github repository?"
 read URL
-
-echo $PATH
-echo $PROJECT
 echo $URL
+
+yes_no_input "Does this work?"
+echo $answer
